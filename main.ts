f59d1fbadd4e2f734e6c9346770756d0f8160e12
DrCashew31 <113105239+DrCashew31@users.noreply.github.com>
2022-09-22 15:48:47 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,38 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-    	
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M3,
+        -255,
+        SuperBit.enMotors.M4,
+        255
+        )
     } else if (receivedNumber == 2) {
-    	
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M3,
+        255,
+        SuperBit.enMotors.M4,
+        -255
+        )
     } else if (receivedNumber == 3) {
-    	
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M3,
+        0,
+        SuperBit.enMotors.M4,
+        -255
+        )
     } else if (receivedNumber == 4) {
-    	
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M3,
+        255,
+        SuperBit.enMotors.M4,
+        0
+        )
     } else if (receivedNumber == 5) {
-    	
+        SuperBit.MotorStopAll()
+    } else if (receivedNumber == 6) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
     } else {
-    	
+        SuperBit.MotorStopAll()
     }
 })
 radio.setGroup(10)
