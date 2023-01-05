7e06448f9456c6b0dbb7adbcbd1bc232d1c772ba
DrCashew31 <113105239+DrCashew31@users.noreply.github.com>
2023-01-05 13:21:00 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,30 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        255,
         SuperBit.enMotors.M3,
-        -255,
-        SuperBit.enMotors.M4,
         255
         )
     } else if (receivedNumber == 2) {
         SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        -255,
         SuperBit.enMotors.M3,
-        255,
-        SuperBit.enMotors.M4,
         -255
         )
     } else if (receivedNumber == 3) {
         SuperBit.MotorRunDual(
-        SuperBit.enMotors.M3,
+        SuperBit.enMotors.M1,
         0,
-        SuperBit.enMotors.M4,
-        -255
+        SuperBit.enMotors.M3,
+        255
         )
     } else if (receivedNumber == 4) {
         SuperBit.MotorRunDual(
-        SuperBit.enMotors.M3,
+        SuperBit.enMotors.M1,
         255,
-        SuperBit.enMotors.M4,
+        SuperBit.enMotors.M3,
         0
         )
     } else if (receivedNumber == 5) {
